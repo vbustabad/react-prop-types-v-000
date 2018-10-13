c81fa6334040271ec7c4619b3867e12b20c5e653
Veronica Bustabad <vbustabad@gmail.com>
2018-10-13 17:05:28 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Code Goes Here
+class Order extends React.Component {
+ 
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
